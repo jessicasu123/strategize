039138c13d299afac45ee76e1d44da47debf6c8d
Holly Ansel <holly.ansel@duke.edu>
2020-04-02 19:55:37 -0400

    completed APIs and put in plan directory

---
+++ doc/plan/api/model/data/FileDataException.java
@@ -0,0 +1,9 @@
+package ooga.model.data;
+
+/**
+ * The purpose of this exception is to let the user know if a file
+ * that the entered does not contain the proper data
+ */
+
+public class FileDataException extends Exception {
+}
---
+++ doc/plan/api/model/engine/InvalidMoveException.java
@@ -0,0 +1,8 @@
+package ooga.model.engine;
+/**
+ * The purpose of this exception is to let the user know when they make a move
+ * that is not valid according to the game rules
+ */
+
+public class InvalidMoveException extends Exception {
+}
