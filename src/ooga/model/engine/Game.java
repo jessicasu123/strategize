f948425245bb2210f30c986655f1c2af80d8c942
Jessica Su <js803@duke.edu>
2020-04-05 18:11:54 -0700

    constructor & base methods for Game

--- src/ooga/model/engine/Game.java
+++ src/ooga/model/engine/Game.java
@@ -1,4 +1,26 @@
 package ooga.model.engine;
 
-public class Game {
+import java.util.List;
+
+public class Game implements GameFramework{
+    BoardFramework myBoard;
+
+    public Game(String gameType, List<List<Integer>> startingConfiguration) {
+        myBoard = new Board(gameType, startingConfiguration);
+    }
+
+    @Override
+    public void makeAgentMove() throws InvalidMoveException {
+
+    }
+
+    @Override
+    public int getEndGameStatus() {
+        return 0;
+    }
+
+    @Override
+    public List<List<Integer>> getVisualInfo() {
+        return myBoard.getStateInfo();
+    }
 }
