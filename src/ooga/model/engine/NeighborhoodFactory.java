8d45c114feb884c08bac4bf35f3c1b13065f6749
Jessica Su <js803@duke.edu>
2020-04-07 12:43:06 -0700

    framework for neighborhood factory

---
+++ src/ooga/model/engine/NeighborhoodFactory.java
@@ -0,0 +1,8 @@
+package ooga.model.engine;
+
+public class NeighborhoodFactory {
+    public Neighborhood createNeighborhood(String neighborhoodType, int numRows, int numCols) {
+        return null;
+    }
+
+}
