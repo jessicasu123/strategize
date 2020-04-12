1ae6ebd13e4b8fbb01d817a699c7c18e8c900cb1
Holly Ansel <holly.ansel@duke.edu>
2020-04-12 18:32:50 -0400

    fixing error in test suite

--- test/ooga/model/Data/JSONFileReaderTest.java
+++ test/ooga/model/Data/JSONFileReaderTest.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JSONFileReaderTest {
-    private String fileNameGame = "resources/tic-tac-toe.json";
+    private String fileNameGame = "tic-tac-toe.json";
     JSONFileReader fr = new JSONFileReader(fileNameGame);
 
     JSONFileReaderTest() throws FileNotFoundException {
