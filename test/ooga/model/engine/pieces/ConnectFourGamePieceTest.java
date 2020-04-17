cd04a136a68e2a9b959d82cf0e572aa2a0e92573
Brian Li <bl195@duke.edu>
2020-04-16 23:14:00 -0400

    connect4 tests done

--- test/ooga/model/engine/pieces/ConnectFourGamePieceTest.java
+++ test/ooga/model/engine/pieces/ConnectFourGamePieceTest.java
@@ -1,16 +1,45 @@
 package ooga.model.engine.pieces;
 
+import ooga.model.engine.Coordinate;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConnectFourGamePieceTest {
+    Coordinate c = new Coordinate(5,3);
+    GamePiece piece = new ConnectFourGamePiece(0,c);
+    Coordinate c1 = new Coordinate(5,3);
+    GamePiece neighbor = new ConnectFourGamePiece(2,c1);
 
     @Test
-    void calculateAllPossibleMoves() {
+    void testCalculateAllPossibleMoves() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(c1);
+        List<GamePiece> dummyNieghbors = new ArrayList<>();
+        dummyNieghbors.add(neighbor);
+        assertEquals(coords, piece.calculateAllPossibleMoves(dummyNieghbors,1));
     }
 
     @Test
-    void makeMove() {
+    void testMakeMove() {
+        Coordinate endCoordinate = new Coordinate(5,2);
+        List<GamePiece> dummyNieghbors = new ArrayList<>();
+        int newState = 1;
+        piece.makeMove(endCoordinate,dummyNieghbors,newState);
+        assertEquals(endCoordinate, piece.getPosition());
+        assertEquals(newState,piece.getState());
+    }
+
+    @Test
+    void testgetState() {
+        assertEquals(0, piece.getState());
+    }
+
+    @Test
+    void testGetPosition() {
+        assertEquals(c, piece.getPosition());
     }
 }
