cdf8081d110b077c00e29b47ea18057f421acdf8
Brian Li <bl195@duke.edu>
2020-04-16 22:45:11 -0400

    added connect4 test classes

---
+++ test/ooga/model/engine/Agent/ConnectFourAgentTest.java
@@ -0,0 +1,18 @@
+package ooga.model.engine.Agent;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectFourAgentTest {
+    private ConnectFourAgent myConnectAgent = new ConnectFourAgent(1,2,4);
+
+    @Test
+    void testEvaluateCurrentGameState() {
+        
+    }
+
+    @Test
+    void testisWin() {
+    }
+}
