a4e95a3bd2fb9f45867102bc86868036da857967
Jessica Su <js803@duke.edu>
2020-04-19 13:35:51 -0700

    adding parameters to position weights in factory

--- src/ooga/model/engine/Agent/newAgent/evaluationFunctions/EvaluationFunctionFactory.java
+++ src/ooga/model/engine/Agent/newAgent/evaluationFunctions/EvaluationFunctionFactory.java
@@ -8,12 +8,13 @@ import java.util.List;
 public class EvaluationFunctionFactory {
     //TODO: do we want to have weights for each of the evaluation functions
     public EvaluationFunction createEvaluationFunction(String evaluationType, int maxState, int minState,
-                                                 List<Integer> maxStates, List<Integer> minStates, List<List<Integer>> boardWeights) {
+                                                 List<Integer> maxStates, List<Integer> minStates, List<List<Integer>> boardWeights,
+                                                       int maxDirection, int minDirection) {
         switch (evaluationType) {
             case "SumOfDistancesForSpecialPiece":
                 return new SumOfDistancesForSpecialPieceEval(maxState, minState, maxStates, minStates);
             case "PositionWeights":
-                return new PositionWeights(boardWeights,maxStates,minStates);
+                return new PositionWeights(boardWeights,maxStates,minStates,maxDirection, minDirection);
             default:
                 throw new InvalidEvaluationFunctionException(evaluationType + " is not a valid neighborhood type.");
         }
