94d5509f146c8a55b3c3f2c3260cc9598438b891
Sanya Kochhar <sk489@duke.edu>
2020-04-20 15:51:51 -0400

    Updated wintype factory for nomovesmorepieces

--- src/ooga/model/engine/Agent/newAgent/winTypes/WinTypeFactory.java
+++ src/ooga/model/engine/Agent/newAgent/winTypes/WinTypeFactory.java
@@ -2,16 +2,19 @@ package ooga.model.engine.Agent.newAgent.winTypes;
 
 import ooga.model.engine.exceptions.InvalidWinTypeException;
 
+import java.util.List;
+
 public class WinTypeFactory {
 
-    public WinType createWinType(String winType, int emptyState, int winValue, int playerRow, int myPlayer) {
+    public WinType createWinType(String winType, int emptyState, int winValue, int playerRow, int myPlayer, int playerStateIndex,
+                                 List<List<Integer>> initialConfig, Boolean checkCurrConfig) {
         switch (winType) {
             case "ConsecutivePieces":
                 return new ConsecutivePieces(winValue, myPlayer);
             case "NoPiecesForOpponent":
                 return new NoPiecesForOpponent(emptyState);
             case "NoMovesMorePieces":
-                return new NoMovesMorePieces(winValue, playerRow);
+                return new NoMovesMorePieces(winValue, playerStateIndex, initialConfig, checkCurrConfig);
             default:
                 throw new InvalidWinTypeException(winType + " is not a valid neighborhood type.");
         }
