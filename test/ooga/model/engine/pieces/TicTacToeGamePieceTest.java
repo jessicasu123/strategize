1dd710d8852159d26933030eb3b191f40c24cae0
Sanya Kochhar <sk489@duke.edu>
2020-04-22 10:49:55 -0400

    Updates to tictactoe gamepiece test

--- test/ooga/model/engine/pieces/TicTacToeGamePieceTest.java
+++ test/ooga/model/engine/pieces/TicTacToeGamePieceTest.java
@@ -9,12 +9,23 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeGamePieceTest {
-    Coordinate c = new Coordinate(0,1);
-    GamePiece piece = new TicTacToeGamePiece(0, c,1);
+    Coordinate emptyCoord = new Coordinate(0,1);
+    Coordinate xCoord = new Coordinate(0,0);
+    Coordinate oCoord = new Coordinate(0,2);
+    GamePiece emptyPiece = new TicTacToeGamePiece(0, emptyCoord,1);
+    GamePiece xPiece = new TicTacToeGamePiece(1, xCoord,1);
+    GamePiece oPiece = new ConnectFourGamePiece(2, oCoord,1);
+    List<GamePiece> dummyNeighbors = List.of(xPiece, oPiece);
 
     @Test
     void testCalculateAllPossibleMoves() {
-        // TODO: update this depending on whether we decide to return just that piece's position or all positions
+        // possible moves for empty cell
+        List<Coordinate> possibleMoves = emptyPiece.calculateAllPossibleMoves(dummyNeighbors, 1);
+        assertEquals(List.of(emptyCoord), possibleMoves);
+
+        // possible moves for occupied cell
+        List<Coordinate> noPossibleMoves = xPiece.calculateAllPossibleMoves(dummyNeighbors, 1);
+        assertEquals(List.of(), noPossibleMoves);
     }
 
     @Test
@@ -22,18 +33,23 @@ class TicTacToeGamePieceTest {
         Coordinate endCoordinate = new Coordinate(1,1);
         List<GamePiece> dummyNeighbors = new ArrayList<>();
         int newState = 1;
-        piece.makeMove(endCoordinate, dummyNeighbors, newState);
-        assertEquals(endCoordinate, piece.getPosition());
-        assertEquals(newState, piece.getState());
+        emptyPiece.makeMove(endCoordinate, dummyNeighbors, newState);
+        assertEquals(endCoordinate, emptyPiece.getPosition());
+        assertEquals(newState, emptyPiece.getState());
     }
+
 
     @Test
     void testGetState() {
-        assertEquals(0, piece.getState());
+        assertEquals(0, emptyPiece.getState());
+        assertEquals(1, xPiece.getState());
+        assertEquals(2, oPiece.getState());
     }
 
     @Test
     void testGetPosition() {
-        assertEquals(c, piece.getPosition());
+        assertEquals(emptyCoord, emptyPiece.getPosition());
+        assertEquals(xCoord, xPiece.getPosition());
+        assertEquals(oCoord, oPiece.getPosition());
     }
 }
