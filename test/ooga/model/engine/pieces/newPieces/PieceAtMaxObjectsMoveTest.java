adab012e7cd11764e89b4541a10ed960164bc51b
Sanya Kochhar <sk489@duke.edu>
2020-04-24 18:41:03 -0400

    Added tests for SplitObjectMove

--- test/ooga/model/engine/pieces/newPieces/PieceAtMaxObjectsMoveTest.java
+++ test/ooga/model/engine/pieces/newPieces/PieceAtMaxObjectsMoveTest.java
@@ -18,7 +18,7 @@ class PieceAtMaxObjectsMoveTest {
     Coordinate c3 = new Coordinate(1,1);
 
     List<Integer> directions = new ArrayList<>(List.of(1));
-    GamePiece selfPiece = new GamePiece(1, c1,3, directions, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+    GamePiece selfPiece1 = new GamePiece(1, c1,3, directions, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
     GamePiece selfPiece2 = new GamePiece(1, c2,5, directions, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
     GamePiece opponentPiece1 = new GamePiece(2, c3,1, directions, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
     List<GamePiece> neighbors = new ArrayList<>(List.of(selfPiece2, opponentPiece1));
@@ -26,14 +26,14 @@ class PieceAtMaxObjectsMoveTest {
     @Test
     void completeMoveType() {
         // if neighbor piece has max objects, it's state and objects are nullified
-        pieceAtMaxObjectsMove.completeMoveType(selfPiece, c2, neighbors, 1, 1);
+        pieceAtMaxObjectsMove.completeMoveType(selfPiece1, c2, neighbors, 1, 1);
         assertEquals(0, selfPiece2.getState());
         assertEquals(0, selfPiece2.getNumObjects());
-        assertEquals(1, selfPiece.getState());
-        assertEquals(3, selfPiece.getNumObjects());
+        assertEquals(1, selfPiece1.getState());
+        assertEquals(3, selfPiece1.getNumObjects());
 
         // else: it is not modified
-        pieceAtMaxObjectsMove.completeMoveType(selfPiece, c3, neighbors, 1, 1);
+        pieceAtMaxObjectsMove.completeMoveType(selfPiece1, c3, neighbors, 1, 1);
         assertEquals(1, opponentPiece1.getNumObjects());
         assertEquals(2, opponentPiece1.getState());
     }
