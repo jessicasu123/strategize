bb66755fdb81ca399aa32c9784ef911232b35957
Holly Ansel <holly.ansel@duke.edu>
2020-04-23 21:23:56 -0400

    updating check own piece

--- src/ooga/model/engine/pieces/newPieces/MoveChecks/CheckOwnPiece.java
+++ src/ooga/model/engine/pieces/newPieces/MoveChecks/CheckOwnPiece.java
@@ -6,12 +6,14 @@ import ooga.model.engine.pieces.newPieces.GamePiece;
 import java.util.List;
 
 public class CheckOwnPiece implements MoveCheck{
+    private List<Integer> myPlayerStates;
 
-    public CheckOwnPiece(){
+    public CheckOwnPiece(List<Integer> playerStates){
+        myPlayerStates = playerStates;
     }
 
     @Override
     public boolean isConditionMet(Coordinate startingLocation, GamePiece checking, List<GamePiece> neighbors, int state, List<Integer> directions) {
-        return checking.getState() == state;
+        return myPlayerStates.contains(checking.getState());
     }
 }
