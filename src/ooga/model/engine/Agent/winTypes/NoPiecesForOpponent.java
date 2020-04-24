5c7587fe0a9ed0c08f46b30041b6582f3ec37f2d
Holly Ansel <holly.ansel@duke.edu>
2020-04-24 13:14:34 -0400

    adding comments to agent classes

--- src/ooga/model/engine/Agent/winTypes/NoPiecesForOpponent.java
+++ src/ooga/model/engine/Agent/winTypes/NoPiecesForOpponent.java
@@ -2,13 +2,30 @@ package ooga.model.engine.Agent.winTypes;
 
 import java.util.List;
 
+/**
+ * Given the empty state, this class determines that a win for the player
+ * whose states are given if the board only contains their pieces and the empty state
+ * (their opponent has no pieces)
+ * @author Holly Ansel
+ */
+
 public class NoPiecesForOpponent implements WinType {
     private final int myEmptyState;
-    
+
+    /**
+     * @param emptyState - the empty state, used to see if states other than the players and
+     *                   the empty state are on the board
+     */
     public NoPiecesForOpponent(int emptyState){
         myEmptyState = emptyState;
     }
 
+    /**
+     * @param playerStates - a list of the states corresponding to the player
+     * @param boardStateInfo - the current state of the board
+     * @param noMovesLeft - true if there are no moves left, false if there are
+     * @return whether opponent states (states other than the players and the empty state) are on the board
+     */
     @Override
     public boolean isWin(List<Integer> playerStates, List<List<Integer>> boardStateInfo, boolean noMovesLeft) {
         int numOfOpponentsPieces = 0;
